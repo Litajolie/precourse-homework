e4d147597a333de19cdf3a7707ccc184438e8ee5
Kaija D <Kaija@Fambayes-MacBook-Air.local>
2022-10-17 22:02:50 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,28 @@
+computer = "Macbook"
+print(computer)
+codeclan_students = 20
+print(codeclan_students)
+Flat = "home"
+print (Flat) 
+computer =input ("'Do you want a PC?'")
+
+def greet():
+    print ("Hello")
+    print ("How are you PC?")
+
+greet()
+greet()
+greet()
+
+x= 5
+result = x + 10
+print(result)
+
+v = 4
+result = v * 5
+print(result)
+
+person 1 = "Michelle"
+person 2 = "Obama"
+print(person 1 + person 2)
+
